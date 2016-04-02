b53a4995a4c93af2d02f32120eb0b36aa671d37c
HugoRodd <hugorodhugord@outlook.com>
2016-04-02 17:02:46 +0200

    2ª version

--- Form1.cs
+++ Form1.cs
@@ -12,18 +12,24 @@ namespace ProyectoPruebaCSharp
 {
     public partial class Form1 : Form
     {
+        //Texto que aparece
         public string textoBanner = "Controlado con -git";
+        //Contador que indica el numero de letras que están apareciendo en cada intervalo de timer1
         public short index = 0;
         public Form1()
         {
             InitializeComponent();
         }
-
+        //Nada más cargarse el formulario, empieza el contador
         private void Form1_Load(object sender, EventArgs e)
         {
             timer1.Enabled = true;
         }
-
+        /// <summary>
+        /// Se aplica Substring al texto. Se podría utilizar un indice como en un array.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void timer1_Tick(object sender, EventArgs e)
         {
             label1.Text = textoBanner.Substring(0, index);
